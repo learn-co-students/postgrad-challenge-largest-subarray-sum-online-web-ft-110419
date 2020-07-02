12ee76fd8ddf893ed0fde4edcd1a957402032e07
Bryant Richards <dragonseyeimages@gmail.com>
2020-07-02 10:31:14 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function largestSubarraySum(array) {
+  let currentSum = 0
+  let largestSum = 0
+
+  for(let number of array){
+    currentSum = Math.max(0, (currentSum + number))
+    largestSum = Math.max(largestSum, currentSum)
+  }
+  return largestSum
+
+}
+
+largestSubarraySum(array)
